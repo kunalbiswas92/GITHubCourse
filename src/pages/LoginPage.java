254e42ec4823fd44089d1bc1e88a6ed81431055e
kunalbiswas92 <jsmith.u1@gmail.com>
2022-05-03 20:03:22 +0530

    updated/2nd commit

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -5,5 +5,8 @@ public void test1()
 
 {
 	System.out.println("Test1 - LP");
+	String user = "admin";
+	String pasword = "admin";
+	
 }
 }
--- src/tests/HomePageTest.java
+++
@@ -1,8 +0,0 @@
-package tests;
-
-public class HomePageTest {
-	public static void main(String[] args)
-	{
-		System.out.println("Login page test");
-	}
-}
